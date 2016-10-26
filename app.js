d22cc8c5afcdaf6b16272f7ab088351339af5d37
Cayonliow <cayon.1318.96@hotmail.com>
2016-10-27 00:56:02 +0800

    26/10

--- app.js
+++ app.js
@@ -2,14 +2,17 @@ $(document).ready(function(){
 
   $("#foo1").hide();
   $("#foo2").hide();
+  $("#foo3").hide();
   $("#showsomething").click(function(){
     $("#foo1").slideToggle();
   });
-  
-
 
   $("#showfacebook").click(function(){
     $("#foo2").slideToggle();
+  });
+
+  $("#showig").click(function(){
+    $("#foo3").slideToggle();
   });
 
 
